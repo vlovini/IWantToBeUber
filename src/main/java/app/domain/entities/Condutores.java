2d4c9d171561da984ca95e084aba072f1397b665
bbockric <52682157+bbockric@users.noreply.github.com>
2020-07-04 15:33:29 -0300

    Atualização do Arquivo Condutores
    Eu tinha sobrescrito o projeto original com um arquivo desatualizado meu.
    
    Estou repondo o arquivo com os comentários do Vinícius

--- src/main/java/app/domain/entities/Condutores.java
+++ src/main/java/app/domain/entities/Condutores.java
@@ -9,6 +9,13 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+<<<<<<< HEAD
+=======
+/*! \brief Projeção JPA para aplicação do conjunto de entidades "Condutores" no Banco de dados - Herda atributos básicos da classe "AbstractPessoa" */
+/*! \author Vinícius Lora, José Ricardo Bock */
+/*! \since  28/05/2020 */
+/*! \date  31/05/2020 */
+>>>>>>> dfa6bfa7c1eaf5b8c615d640b64196325ed3712f
 
 @Entity(name = "Condutores")
 @Table(name = "CONDUTORES")
@@ -23,7 +30,11 @@ public class Condutores extends AbstractPessoa {
     @Id
     @Column(name = "CPF_CONDUTOR")
     @JsonProperty("cpfCondutor")
+<<<<<<< HEAD
 /*! @param cpfCondutor é uma primary key */
+=======
+    /** Chave primaria da tabela Condutores */
+>>>>>>> dfa6bfa7c1eaf5b8c615d640b64196325ed3712f
     private String cpfCondutor;
 /**
  * Método para invocar o CPF de um condutor
